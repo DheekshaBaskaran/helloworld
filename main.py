8e1506d704de2db0fe41034e7dd3383f3c54a47f
dheekshabaskaran <dheeksha.bas@gmail.com>
2024-08-01 16:38:16 -0700

    hello world

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-# index.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def hello_world():
     return render_template('index.html')
 
 if __name__ == '__main__':
